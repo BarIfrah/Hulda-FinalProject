a7bfe57ff08b623fd3508c86513bd56e0a3f4183
Bar <ibari02@gmail.com>
2021-06-06 11:13:37 +0300

    add Tel Aviv background. Add Up/Down movements. Need to work on image scaling of side scroll.

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -15,10 +15,11 @@ void Controller::run() {
     sf::RectangleShape background;
     background.setTexture(&Resources::instance()
             .getBackground(0));
-    background.setSize(sf::Vector2f(m_window.getSize()));
+    background.setSize(sf::Vector2f(7185, 1920));
     sf::RectangleShape rec;
     rec.setFillColor(sf::Color::Blue);
     rec.setSize(sf::Vector2f(100, 100));
+    rec.setPosition(sf::Vector2f(0, 1600));
     //-------------------------------------------------------
     while (m_window.isOpen())
     {
@@ -59,13 +60,17 @@ void Controller::run() {
                 default:
                     ;
             }
-            m_CurrViewPos.x = rec.getPosition().x + 50 - (float(m_window.getSize().x) / 2);
+            m_CurrViewPos.x = rec.getPosition().x + 50 - (float(m_window.getSize().x) / 5);
             m_CurrViewPos.y = rec.getPosition().y + 50 - (float(m_window.getSize().y) / 2);
 
             if (m_CurrViewPos.x < 0)
                 m_CurrViewPos.x = 0;
             if (m_CurrViewPos.y < 0)
                 m_CurrViewPos.y = 0;
+            if (m_CurrViewPos.x > m_window.getSize().x)
+                m_CurrViewPos.x = m_window.getSize().x;
+            if (m_CurrViewPos.y > m_window.getSize().y)
+                m_CurrViewPos.y = m_window.getSize().y;
             m_screenView.reset(sf::FloatRect(m_CurrViewPos.x, m_CurrViewPos.y, m_window.getSize().x, m_window.getSize().y));
             m_window.setView(m_screenView);
         }
