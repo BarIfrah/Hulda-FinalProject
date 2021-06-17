da098a4033b9c6bcbd837ec37ce53c02508d132f
Bar <ibari02@gmail.com>
2021-06-17 16:27:31 +0300

    merge with Box2D-Sharon

--- src/PhysicsObject.cpp
+++ src/PhysicsObject.cpp
@@ -1,33 +1,28 @@
 #include "PhysicsObject.h"
 #include "Macros.h"
 //===========================================================================
-PhysicsObject::PhysicsObject(b2World& world, const sf::Vector2f& position, const bool dynamic, 
-   const sf::Vector2f &size) {
+PhysicsObject::PhysicsObject(b2World& world, const sf::Vector2f& position, const bool dynamic,
+                             const sf::Vector2f &size)
+{
     /*auto objPhysicsInfo = physicsInfo[int(type)];*/
     b2BodyDef bodyDef;
     b2PolygonShape polygonShape;
-    b2CircleShape circleShape;  // if want to add circleshape later
+    //b2CircleShape circleShape;  // if want to add circleshape later
 
-    if (dynamic) {
+    if (dynamic)
         bodyDef.type = b2_dynamicBody;
-        circleShape.m_radius = 0.75 - b2_polygonRadius;
-//        circleShape
-        m_fixtureDef.shape = &circleShape;
-    }else {
+    else
         bodyDef.type = b2_staticBody;
-        polygonShape.SetAsBox((size.x / 4 * MPP), size.y / 2 * MPP);
-        m_fixtureDef.shape = &polygonShape;
-    }
-    auto xloc = float(position.x * MPP + 0.007812);
-    auto yloc = float(position.y * MPP + 0.007812);
-    bodyDef.position.Set(xloc, yloc);
+
+    bodyDef.position.Set(position.x * MPP, position.y * MPP);
 
     m_body = world.CreateBody(&bodyDef);
 
-
+    polygonShape.SetAsBox(size.x / 4 * MPP, size.y / 2 * MPP);
+    m_fixtureDef.shape = &polygonShape;
     m_fixtureDef.friction = 1; // objPhysicsInfo._friction;
     m_fixtureDef.restitution = 0; // objPhysicsInfo._restitution;
-    m_fixtureDef.density = 3.f; // objPhysicsInfo._density;
+    m_fixtureDef.density = 1; // objPhysicsInfo._density;
     m_fixture = m_body->CreateFixture(&m_fixtureDef);  // to update all parameters defined above in Physicsobj.
     m_body->SetFixedRotation(true);  /// this object will not rotate.
 }
